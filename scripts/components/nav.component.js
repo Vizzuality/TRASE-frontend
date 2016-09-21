f769431faf585c63125f05d24602011031f3f137
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-09-21 18:55:09 +0200

    markup & styles new top menu

--- scripts/components/nav.component.js
+++ scripts/components/nav.component.js
@@ -6,8 +6,15 @@ import ShareMenu from 'components/nav/share/share-menu.component';
 export default class {
   onCreated() {
 
+    // left side
     this.countryDropdown = new Dropdown('country', this.callbacks.onCountrySelected);
+    this.commodityDropdown = new Dropdown('commodity', this.callbacks.onCountrySelected);
+    this.yearDropdown = new Dropdown('year', this.callbacks.onQuantSelected);
+
+    // right side
     this.quantDropdown = new Dropdown('quant', this.callbacks.onQuantSelected);
+    this.colorDropdown = new Dropdown('color', this.callbacks.onQuantSelected);
+    this.viewDropdown = new Dropdown('view', this.callbacks.onQuantSelected);
 
     new BookmarkMenu();
     new Dropdown('bookmark', this.callbacks.onQuantSelected);
