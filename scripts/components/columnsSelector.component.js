d89a6fb7862dc1257ffe3b803167ed0dbc53ab27
nerik <erik.escoffier@gmail.com>
2016-09-26 15:47:57 +0200

    wip

--- scripts/components/columnsSelector.component.js
+++ scripts/components/columnsSelector.component.js
@@ -1,4 +1,14 @@
+import Dropdown from 'components/dropdown.component';
+import 'styles/components/columnsSelector.scss';
+
 export default class {
+  onCreated() {
+    new Dropdown('column0', value => {console.log(value);});
+    new Dropdown('column1', value => {console.log(value);});
+    new Dropdown('column2', value => {console.log(value);});
+    new Dropdown('column3', value => {console.log(value);});
+  }
+
   columnsLoaded(payload) {
     console.log(payload);
   }
