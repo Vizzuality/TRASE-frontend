89f05c860c2028f5b227a708997df71c48a40e2d
nerik <erik.escoffier@gmail.com>
2016-09-27 19:00:33 +0200

    year selector

--- scripts/components/nav/years-brush/years-slider.component.js
+++ scripts/components/nav/years-brush/years-slider.component.js
@@ -1,19 +1,134 @@
-import Brush from 'components/nav/years-brush/brush.component';
+import { select as d3_select } from 'd3-selection';
+import { axisTop as d3_axis_top } from 'd3-axis';
+import { scaleTime as d3_scale_time } from 'd3-scale';
+import { timeYear as d3_time_timeYear } from 'd3-time';
+import { brushX as d3_brush_x } from 'd3-brush';
+import { event as d3_event } from 'd3-selection'
+import ThumbTemplate from 'ejs!templates/years-slider-thumb.ejs';
+import addSVGDropShadowDef from 'utils/addSVGDropShadowDef';
+import {AVAILABLE_YEARS} from 'constants';
+import 'styles/components/nav/years-slider.scss';
+
+
 
 export default class {
   constructor(id, callback) {
     this.id = id;
-    // this.data = data;
     this.callback = callback;
+    this.el = document.querySelector(`.${id}`);
 
-    this.render();
+    this._build();
   }
 
-  render () {
-    return new Brush({
-      id: this.id,
-      data: this.data,
-      callback: this.callback
-    });
+  _build () {
+    const startYear = AVAILABLE_YEARS[0];
+    const endYear = AVAILABLE_YEARS[AVAILABLE_YEARS.length - 1];
+
+    var margin = {
+      top: 5,
+      right: 5,
+      bottom: 10,
+      left: 5
+    };
+    const width = (AVAILABLE_YEARS.length * 40) - margin.left - margin.right;
+    const height = 45 - margin.top - margin.bottom;
+    this.xScale = d3_scale_time()
+      .domain([new Date(startYear, 1, 1), new Date(endYear, 11, 31)])
+      .rangeRound([0, width])
+      .nice(d3_time_timeYear);
+
+    const svg = d3_select(this.el).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom);
+
+    addSVGDropShadowDef(svg);
+
+    this.slider = svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+    this.brushBehavior = d3_brush_x()
+      .extent([[0, 0], [width, height]])
+      .on('brush', this._onBrushMove.bind(this))
+      .on('end', this._onBrushEnd.bind(this));
+
+    this.brush = this.slider.append('g')
+      .attr('class', 'brush')
+      .call(this.brushBehavior);
+
+
+    this._addAxis('axis', height);
+
+
+    this.selectionOverlay = this.slider.append('g').attr('class', 'selectionOverlay');
+
+    this.selectionOverlayRect = this.selectionOverlay.append('rect')
+      .attr('class', 'selectionOverlayRect')
+      .attr('height', height)
+      .style('filter', 'url(#drop-shadow)'); // defined by addSVGDropShadowDef
+
+    this.selectionOverlayThumbLeft = this.selectionOverlay.append('g')
+      .attr('transform', 'translate(-4, -4)')
+      .html(ThumbTemplate());
+    this.selectionOverlayThumbRight = this.selectionOverlay.append('g')
+      .html(ThumbTemplate());
+
+
+    this._addAxis('axisFront', height);
+    this.slider.selectAll('.axisFront .tick text')
+      .attr('x', 19);
   }
+
+  _addAxis(className, height) {
+    this.slider.append('g')
+      .attr('class', className)
+      .attr('transform', `translate(0, ${height})`)
+      .call(d3_axis_top(this.xScale)
+        .ticks(d3_time_timeYear)
+        .tickPadding(-18)
+        .tickSize(height)
+      );
+
+  }
+
+  _onBrushMove() {
+    this._moveBrush(d3_event.selection);
+  }
+
+  _onBrushEnd() {
+    if (!d3_event.sourceEvent) return;
+    if (!d3_event.selection) return;
+
+    var d0 = d3_event.selection.map(this.xScale.invert),
+      d1 = d0.map(d3_time_timeYear.round);
+
+    if (d1[0] >= d1[1]) {
+      d1[0] = d3_time_timeYear.floor(d0[0]);
+      d1[1] = d3_time_timeYear.offset(d0[1]);
+    }
+
+    const pixelSelection = d1.map(this.xScale);
+
+    this._moveBrush(pixelSelection);
+
+    d3_select('.brush').transition()
+      .call(d3_event.target.move, pixelSelection);
+
+    // var startYear = new Date (d1[0]).getFullYear();
+    // var endYear= new Date (d1[1]).getFullYear();
+
+    // callback([startYear, endYear]);
+  }
+
+  _moveBrush(pixelSelection) {
+    const x = pixelSelection[0];
+    const width = pixelSelection[1] - x;
+    this.selectionOverlay.attr('transform', `translate(${x}, 0)`);
+    this.selectionOverlayRect.attr('width', width);
+    this.selectionOverlayThumbRight.attr('transform', `translate(${width-4}, -4)`);
+
+    return pixelSelection;
+  }
+
 }
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,1 +1,2 @@
 export const NUM_COLUMNS = 4;
+export const AVAILABLE_YEARS = [2012, 2013, 2014, 2015, 2016];
