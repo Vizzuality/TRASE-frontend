6933188ac0d1ca74a150c1513d4ef38600f18789
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-10-10 11:46:41 +0200

    removed unnecessary callbacks

--- scripts/containers/nodesTitles.container.js
+++ scripts/containers/nodesTitles.container.js
@@ -7,8 +7,4 @@ const mapMethodsToState = (state) => ({
   update: state.flows.selectedNodesData
 });
 
-const mapViewCallbacksToActions = () => ({
-
-});
-
-export default connect(NodesTitles, mapMethodsToState, mapViewCallbacksToActions);
+export default connect(NodesTitles, mapMethodsToState);
