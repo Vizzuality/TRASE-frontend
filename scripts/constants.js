bbcc974987d8713f0d293a1bd2d788e61574bf78
erik <erik.escoffier@gmail.com>
2016-10-18 16:39:17 +0200

    fixed legend colours

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,8 +2,10 @@ export const NUM_COLUMNS = 4;
 export const NUM_NODES = 10;
 export const AVAILABLE_YEARS = [2010, 2011, 2012, 2013, 2014, 2015, 2016];
 export const LEGEND_COLORS = {
-  bidimensional: ['#8db6ca', '#8f8d94', '#694d52', '#aadad1', '#a8bebf',
-    '#a15661', '#f5f3ea', '#f6c7ad', '#f65e6e'], //9
-  horizontal: ['#8db6ca', '#8f8d94', '#694d52', '#aadad1', '#a8bebf'], // 5
-  vertical: ['#a8bebf', '#a15661', '#f5f3ea', '#f6c7ad', '#f65e6e'] // 5
+  bidimensional: [
+    '#8db6ca', '#8f8d94', '#694d52',
+    '#aadad1', '#a8bebf', '#a15661',
+    '#f5f3ea', '#f6c7ad', '#f65e6e'], //9
+  horizontal: ['#f5f3ea', '#f5dfd0', '#f6c7ad', '#fa898d', '#f65e6e'], // 5
+  vertical: ['#F4F1E7', '#C8E1D8', '#A0D5CB', '#87B3C0', '#7297AA'], // 5
 };
