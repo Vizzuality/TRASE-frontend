8b06d2ae0c6985869b872de5bebc5d77ba424181
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-10-24 12:25:16 +0200

    removed json dependency

--- scripts/pages/index.page.js
+++ scripts/pages/index.page.js
@@ -1,8 +1,6 @@
 
 import Dropdown from 'scripts/components/dropdown.component';
 
-import { json as d3_json } from 'd3-request';
-
 import 'styles/homepage.scss';
 
 const defaults = {
@@ -12,28 +10,20 @@ const defaults = {
 
 const _setMap = function() {
   const imagesItem = document.querySelectorAll('.map-gallery-item');
-  const defaultCommodity = defaults.commodity.toLowerCase();
-  const defaultCountry = defaults.country.toLowerCase();
+  const commodity = defaults.commodity.toLowerCase();
+  const country = defaults.country.toLowerCase();
+  const imageName = `${commodity}-${country}`;
 
-  d3_json('homepage/maps.json', (err, response) => {
-    const res = response.maps.filter((r) => {
-      return r.commodity === defaultCommodity && r.country === defaultCountry;
-    });
-
-    if (!res[0]) return;
-
-    const imageName = res[0]['image_name'];
-
-    imagesItem.forEach((imageItem) => {
-      if (imageItem.getAttribute('data-image-name') === imageName) {
-        imageItem.classList.remove('is-hidden');
-      } else {
-        if (!imageItem.classList.contains('is-hidden')) {
-          imageItem.classList.add('is-hidden');
-        }
+  imagesItem.forEach((imageItem) => {
+    if (imageItem.getAttribute('data-image-name') === imageName) {
+      imageItem.classList.remove('is-hidden');
+    } else {
+      if (!imageItem.classList.contains('is-hidden')) {
+        imageItem.classList.add('is-hidden');
       }
-    });
+    }
   });
+
 };
 
 
