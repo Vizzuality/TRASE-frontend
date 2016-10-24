6cb989b0b28c8946297704d55388f45cd3ca6922
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-10-24 14:12:55 +0200

    CR

--- scripts/pages/index.page.js
+++ scripts/pages/index.page.js
@@ -3,27 +3,20 @@ import Dropdown from 'scripts/components/dropdown.component';
 
 import 'styles/homepage.scss';
 
-const defaults = {
+const mapDefaults = {
   commodity: 'Soy',
   country: 'Brazil'
 };
 
 const _setMap = function() {
   const imagesItem = document.querySelectorAll('.map-gallery-item');
-  const commodity = defaults.commodity.toLowerCase();
-  const country = defaults.country.toLowerCase();
+  const commodity = mapDefaults.commodity.toLowerCase();
+  const country = mapDefaults.country.toLowerCase();
   const imageName = `${commodity}-${country}`;
 
   imagesItem.forEach((imageItem) => {
-    if (imageItem.getAttribute('data-image-name') === imageName) {
-      imageItem.classList.remove('is-hidden');
-    } else {
-      if (!imageItem.classList.contains('is-hidden')) {
-        imageItem.classList.add('is-hidden');
-      }
-    }
+    imageItem.classList.toggle('is-hidden', imageItem.getAttribute('data-image-name') !== imageName);
   });
-
 };
 
 
@@ -31,7 +24,7 @@ const _onSelect = function(value) {
   // updates dropdown's title with new value
   this.setTitle(value);
   // updates default values with incoming ones
-  defaults[this.id] = value;
+  mapDefaults[this.id] = value;
 
   // change map image based on new values
   _setMap();
@@ -41,6 +34,6 @@ const _onSelect = function(value) {
 const commodityDropdown = new Dropdown('commodity', _onSelect);
 const countryDropdown = new Dropdown('country', _onSelect);
 
-commodityDropdown.setTitle(defaults.commodity);
-countryDropdown.setTitle(defaults.country);
+commodityDropdown.setTitle(mapDefaults.commodity);
+countryDropdown.setTitle(mapDefaults.country);
 _setMap();
