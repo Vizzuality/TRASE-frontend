9b679ad537af570c73cbeb0d7b57e0c8ea215107
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-10-27 19:52:14 +0200

    WIP

--- scripts/pages/about.page.js
+++ scripts/pages/about.page.js
@@ -1,3 +1,23 @@
-console.info('You are at contact page');
-
 import 'styles/about.scss';
+
+import smoothScroll from 'utils/smoothScroll';
+
+
+// const _doScroll = (e) => {
+//   e && e.preventDefault();
+//   const target = e.target;
+//
+// };
+
+
+// const _setEventListeners = () => {
+//   const anchorItems = document.querySelectorAll('.anchor-item > a');
+//
+//   anchorItems.forEach((anchorItem) => {
+//     anchorItem.addEventListener('click', (e) => _doScroll(e));
+//   });
+//
+// };
+
+// _setEventListeners();
+smoothScroll();
