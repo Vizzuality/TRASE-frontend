986b17d1186ea4492415cdad19fb4ab5832132dd
erik <erik.escoffier@gmail.com>
2016-10-28 17:27:55 +0200

    Autocomplete on nodes list

---
+++ scripts/components/search.component.js
@@ -0,0 +1,59 @@
+import 'styles/components/search.scss';
+import _ from 'lodash';
+import Awesomplete from 'awesomplete';
+import 'awesomplete/awesomplete.css';
+
+
+export default class {
+  onCreated() {
+    this.el = document.querySelector('.js-search');
+    this.input = this.el.querySelector('.js-search-input');
+    this.autocomplete = new Awesomplete(this.input, {
+      data: node => {
+        return {
+          label: node.name.toLowerCase(),
+          value: JSON.stringify({
+            id: node.id,
+            name: node.name.toLowerCase(),
+            columnName: node.columnName.toLowerCase()
+          })
+        };
+      },
+      item: (text, input) => {
+        let html;
+        if (input === '') {
+          html = text;
+        } else {
+          // next two lines are borrowed from awesomeplete's default ._ITEM implementation
+          const s = input.trim().replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+          html = text.replace(RegExp(s, 'gi'), '<mark>$&</mark>');
+          html = `<span class="nodeName">${html}</span>`;
+
+          const value = JSON.parse(text.value);
+          html += `<span class="nodeType">${value.columnName}</span>`;
+        }
+        const dom = document.createElement('li');
+        dom.innerHTML = html;
+        return dom;
+      },
+      // sets the value that appear in the input when selecting a list item
+      // needs to be overriden because by default it will spit the whole item html
+      replace: text => {
+        this.input.value = text;
+      }
+    });
+
+    this.input.addEventListener('awesomplete-selectcomplete', this._onAutocompleteSelected.bind(this));
+  }
+
+  loadNodes(nodesDict) {
+    const nodesArray = _.values(nodesDict).filter(node => node.isAggregated !== true);
+    this.autocomplete.list = nodesArray;
+  }
+
+  _onAutocompleteSelected(event) {
+    const nodeId = parseInt(JSON.parse(event.text.value).id);
+    this.callbacks.onNodeSelected(nodeId);
+  }
+
+}
