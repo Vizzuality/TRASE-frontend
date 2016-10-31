4ac40cc8b1731254475e2c94674dd5baf2981e23
Hector Arce <hector.arce@vizzuality.com>
2016-10-31 13:33:27 +0100

    add select area on factsheets-page

--- scripts/pages/factsheets.page.js
+++ scripts/pages/factsheets.page.js
@@ -1,13 +1,34 @@
+import Dropdown from 'scripts/components/dropdown.component';
 import 'styles/factsheets.scss';
 
-import AreaStack from 'scripts/components/graphs/area-stack.component';
+// import AreaStack from 'scripts/components/graphs/area-stack.component';
+// const _renderAreaStack = () => {
+//   const el = document.querySelector('.js-municipalities-top');
+//
+//   new AreaStack({
+//     el
+//   });
+// };
 
-const _renderAreaStack = () => {
-  const el = document.querySelector('.js-municipalities-top');
-
-  new AreaStack({
-    el
-  });
+const defaults = {
+  exporter: 'Amaggi',
+  commodity: 'Soy',
 };
 
-_renderAreaStack();
+const _onSelect = function(value) {
+  // updates dropdown's title with new value
+  this.setTitle(value);
+  // updates default values with incoming ones
+  defaults[this.id] = value;
+};
+
+const exporterDropdown = new Dropdown('exporter', _onSelect);
+const commodityDropdown = new Dropdown('commodity', _onSelect);
+
+exporterDropdown.setTitle(defaults.exporter);
+commodityDropdown.setTitle(defaults.commodity);
+
+
+
+
+// _renderAreaStack();
