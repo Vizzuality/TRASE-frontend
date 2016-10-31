1e94bc1619378967122a437f9d262a2068eb8886
erik <erik.escoffier@gmail.com>
2016-10-31 13:43:54 +0100

    basic tiled+utfgrid implementation

--- scripts/components/map.component.js
+++ scripts/components/map.component.js
@@ -1,4 +1,6 @@
 import L from 'leaflet';
+import 'leaflet.utfgrid';
+import 'whatwg-fetch';
 import * as topojson from 'topojson';
 import 'leaflet/dist/leaflet.css';
 import 'style/components/map.scss';
@@ -14,6 +16,50 @@ export default class {
     this.map = L.map('map', mapOptions).setView([-16.20639, -44.43333], 4);
     var layer = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>' });
     this.map.addLayer(layer);
+
+    const username = 'wri-01';
+    const config = {'version': '1.0.1','layers':[{'user_name': 'wri-01','type': 'cartodb','options':{'sql':'SELECT * FROM ptw_grid_only_scoreover0','cartocss':'#layer{polygon-fill: #FF6600; polygon-opacity: 0.7; line-color: #FFF; line-width: 0; line-opacity:1;}','cartocss_version':'2.3.0','interactivity':['cartodb_id','grid_id']}}]};
+
+    fetch(`https://wri-01.cartodb.com/api/v1/map?config=${encodeURIComponent(JSON.stringify(config))}`)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data);
+        function getUrl(format) {
+          let url = 'https://' + username +
+            '.cartodb.com/api/v1/map/' + data.layergroupid +
+            '/0/{z}/{x}/{y}.' + format;
+
+          if (format === 'grid.json') {
+            return url + '?callback={cb}';
+          }
+
+          return url;
+        }
+
+        const layer = new L.tileLayer(getUrl('png'));
+        const utfGrid = new L.UtfGrid(getUrl('grid.json'));
+
+        this.map.addLayer(layer);
+        this.map.addLayer(utfGrid, {
+          resolution: 2
+        });
+
+        utfGrid.on('mouseover', function (e) {
+          if (e.data && e.data.hasOwnProperty('grid_id')) {
+            console.log(e.data.grid_id);
+          }
+        });
+      });
+
+    // $.ajax({
+    //     url: 'https://wri-01.cartodb.com/api/v1/map',
+    //     data: {
+    //       config: decodeURIComponent(JSON.stringify(config))
+    //     },
+    //     success: (data) => { addLayer(data) }
+    //   });
+
+
 
     this._setEventListeners();
   }
