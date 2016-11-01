40b977bf883244dd75bdbf1f7a7439c3a3f8ccd1
nerik <erik.escoffier@gmail.com>
2016-11-01 10:01:52 +0100

    set up correct link to factsheets in sankey

--- scripts/reducers/sankey/getNodesDict.js
+++ scripts/reducers/sankey/getNodesDict.js
@@ -5,6 +5,7 @@ export default function (rawNodes, columns /*, nodesMeta*/) {
   rawNodes.forEach(node => {
     const columnId = node.columnId;
     const column = columns.find(column => column.id === columnId);
+
     const newNode = {
       id: node.id,
       columnId: parseInt(node.columnId),
@@ -14,8 +15,16 @@ export default function (rawNodes, columns /*, nodesMeta*/) {
       geoId: node.geoId,
       inds: []
     };
+
     if (node.isAggregated === true || node.isAggregated === 'true') {
       newNode.isAggregated = true;
+    }
+
+    // See https://github.com/sei-international/TRASE/issues/112
+    if ([0,1,2,4,5].indexOf(newNode.columnId) > -1) {
+      // TODO rename factsheets to factsheet-actor
+      const baseURL = ([0,1,2].indexOf(newNode.columnId) > -1) ? 'factsheet-place' : 'factsheets';
+      newNode.link = `./${baseURL}.html?nodeId=${newNode.id}`;
     }
 
     nodesDict[parseInt(node.id)] = newNode;
