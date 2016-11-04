c4798a8b3651a6220daacc9866a7fe12b0fe634e
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-11-04 12:45:18 +0100

    markup, styles and basic logic

--- scripts/pages/factsheet-place.page.js
+++ scripts/pages/factsheet-place.page.js
@@ -1,20 +1,22 @@
 import 'whatwg-fetch';
-import 'styles/factsheet-place.scss';
+
 import 'styles/_base.scss';
 import 'styles/_texts.scss';
+import 'styles/layouts/l-factsheets-place.scss';
+import 'styles/components/dropdown.scss';
+import 'styles/components/button.scss';
 import 'styles/components/shared/nav.scss';
 import 'styles/components/shared/_footer.scss';
+
+import Dropdown from 'components/dropdown.component';
 import Line from 'components/graphs/line.component';
 import Chord from 'components/graphs/chord.component';
 import Top10 from 'components/graphs/top10.component';
 
-const nodeId = location.search.substr(1).replace('nodeId=','');
-
-fetch(`${API_URL}/v1/get_place_node_attributes?node_id=${nodeId}&country=BRAZIL&commodity=SOY`)
-  .then(response => response.json())
-  .then((data) => {
-    _build(data.data);
-  });
+const defaults = {
+  exporter: 'Brazil',
+  commodity: 'Soy',
+};
 
 const _build = data => {
   new Line('.js-line', data.trajectory_deforestation, data.trajectory_production);
@@ -23,3 +25,29 @@ const _build = data => {
   new Top10(document.querySelector('.js-top10-consumers'), 'Top consumers', data.top_consumers);
   new Chord('.js-chord-consumers', data.top_consumers_matrix, data.top_consumers, data.municip_name);
 };
+
+const _onSelect = function(value) {
+  // updates dropdown's title with new value
+  this.setTitle(value);
+  // updates default values with incoming ones
+  defaults[this.id] = value;
+};
+
+const _init = () => {
+  const nodeId = location.search.substr(1).replace('nodeId=','');
+
+  const municipalityDropdown = new Dropdown('municipality', _onSelect);
+  const commodityDropdown = new Dropdown('commodity', _onSelect);
+
+  municipalityDropdown.setTitle(defaults.exporter);
+  commodityDropdown.setTitle(defaults.commodity);
+
+  fetch(`${API_URL}/v1/get_place_node_attributes?node_id=${nodeId}&country=BRAZIL&commodity=SOY`)
+    .then(response => response.json())
+    .then((data) => {
+      _build(data.data);
+    });
+
+};
+
+_init();
