fba7a5b1c41cb63d0c7db93757ca28df57ef9f61
Hector Arce <hector.arce@vizzuality.com>
2016-11-04 11:35:28 +0100

    improve logic for table component and create json file

--- scripts/components/table/table.component.js
+++ scripts/components/table/table.component.js
@@ -1,223 +1,32 @@
 import TableTemplate from 'ejs!templates/table/table.ejs';
 import TableTopTemplate from 'ejs!templates/table/tableTop.ejs';
-// import { json as d3_json } from 'd3-request';
+import 'whatwg-fetch';
 
 export default class {
   constructor(value) {
+    this.data = null;
+    this.getData()
+      .then(response => response.json())
+      .then((json) => {
+        this.data = json;
+        this.render();
+      });
+  }
 
-    var actors = [
-      {
-        title: [
-          {
-            'name': 'municipallities',
-            'units': ''
-          },
-          {
-            'name': 'TRADE AMOUNT',
-            'units': 'tons'
-          },
-          {
-            'name': 'DEFORESTATION',
-            'units': 'ha'
-          },
-          {
-            'name': 'WATER SCARCITY',
-            'units': 'mm/ha'
-          },
-          {
-            'name': 'CONFLICTS',
-            'units': ''
-          },
-          {
-            'name': 'FIRES',
-            'units': ''
-          }
-        ]
-      },
-      {
-        data: [
-          {
-            'municipalities': [
-              {
-                'value': 'Quêrencia',
-                'unit': ''
-              }
-            ],
-            'trade': [
-              {
-                'value': '2,864',
-                'unit': 'tons'
-              }
-            ],
-            'deforastation': [
-              {
-                'value': '53',
-                'unit': 'ha'
-              }
-            ],
-            'water': [
-              {
-                'value': '53',
-                'unit': 'mm/ha'
-              }
-            ],
-            'conflict': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ],
-            'fire': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ]
-          },
-          {
-            'municipalities': [
-              {
-                'value': 'Quêrencia',
-                'unit': ''
-              }
-            ],
-            'trade': [
-              {
-                'value': '2,863',
-                'unit': 'tons'
-              }
-            ],
-            'deforastation': [
-              {
-                'value': '53',
-                'unit': 'ha'
-              }
-            ],
-            'water': [
-              {
-                'value': '53',
-                'unit': 'mm/ha'
-              }
-            ],
-            'conflict': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ],
-            'fire': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ]
-          },
-          {
-            'municipalities': [
-              {
-                'value': 'Quêrencia',
-                'unit': ''
-              }
-            ],
-            'trade': [
-              {
-                'value': '2,863',
-                'unit': 'tons'
-              }
-            ],
-            'deforastation': [
-              {
-                'value': '53',
-                'unit': 'ha'
-              }
-            ],
-            'water': [
-              {
-                'value': '53',
-                'unit': 'mm/ha'
-              }
-            ],
-            'conflict': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ],
-            'fire': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ]
-          },
-          {
-            'municipalities': [
-              {
-                'value': 'Quêrencia',
-                'unit': ''
-              }
-            ],
-            'trade': [
-              {
-                'value': '2,863',
-                'unit': 'tons'
-              }
-            ],
-            'deforastation': [
-              {
-                'value': '53',
-                'unit': 'ha'
-              }
-            ],
-            'water': [
-              {
-                'value': '53',
-                'unit': 'mm/ha'
-              }
-            ],
-            'conflict': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ],
-            'fire': [
-              {
-                'value': '53',
-                'unit': ''
-              }
-            ]
-          }
-        ]
-      }
-    ];
-
-    var consumers = [
-      {
-        trader:[
-          {
-            'name': 'Craig',
-            'value' : '33'
-          }
-        ]
-      }];
-
-    if(value === 'top'){
-      this.data = consumers;
-    }
-    else {
-      this.data = actors;
-    }
-    this.render(value);
+  getData() {
+    return fetch('factsheets/table.json');
   }
 
   render(value) {
+    var areamunicipalities = document.querySelector('.js-municipalities-table');
+
     if(value === 'top'){
       const template = TableTopTemplate({actors: this.data});
-      document.querySelector('.contain-table').innerHTML = template;
+      areamunicipalities.innerHTML = template;
     }else {
+      console.log(this.data);
       const template = TableTemplate({actors: this.data});
-      document.querySelector('.contain-table').innerHTML = template;
+      areamunicipalities.innerHTML = template;
     }
   }
 }
