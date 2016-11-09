2ed7baf82203d26302b48eca0fbbbde804d96e42
Andrés González Muñoz <andres.gnlez@gmail.com>
2016-11-09 17:26:59 +0100

    added auto-complete search to factsheets page

--- scripts/pages/factsheet-actor.page.js
+++ scripts/pages/factsheet-actor.page.js
@@ -11,7 +11,6 @@ import 'styles/components/factsheets/area-select.scss';
 import Dropdown from 'components/dropdown.component';
 import AreaStack from 'components/graphs/area-stack.component';
 import Table from 'components/table/table.component';
-import Search from 'components/search.component.js';
 
 const _renderAreaStack = () => {
   const el = document.querySelector('.js-municipalities-top');
@@ -41,49 +40,6 @@ const _onSelect = function(value) {
   defaults[this.id] = value;
 };
 
-const parseNode = (node) => {
-  return {
-    label: node.name.toLowerCase(),
-    value: JSON.stringify({
-      id: node.id,
-      name: node.name.toLowerCase(),
-      columnName: node.type.toLowerCase()
-    })
-  };
-};
-
-
-fetch('http://traseplatform.org/api/v1/get_all_nodes?&country=brazil&commodity=soy')
-  .then(response => response.json())
-  .then((result) => {
-    const search = new Search();
-    search.onCreated();
-
-    search.autocomplete.data = parseNode;
-    search.loadNodes(result.data);
-    search.callbacks = {};
-    search.callbacks.onNodeSelected = function() {
-      let url = null;
-      const nodeId = parseInt(JSON.parse(event.text.value).id);
-      const type = JSON.parse(event.text.value).columnName;
-
-      if (type === 'municipality'
-        || type === 'biomes'
-        || type === 'state') {
-
-        url = `factsheet-place.html?nodeId=${nodeId}`;
-      }
-
-      if (type === 'exporter' || type === 'importer') {
-        url = `factsheets.html?nodeId=${nodeId}`;
-      }
-
-      if (!url) return;
-
-      window.location.href = url;
-    };
-
-  });
 
 const exporterDropdown = new Dropdown('exporter', _onSelect);
 const commodityDropdown = new Dropdown('commodity', _onSelect);
--- scripts/pages/factsheets.page.js
+++ scripts/pages/factsheets.page.js
@@ -4,4 +4,65 @@ import 'styles/layouts/l-factsheets.scss';
 import 'styles/components/shared/nav.scss';
 import 'styles/components/shared/_footer.scss';
 
-console.info('factsheets page');
+import _ from 'lodash';
+
+import Search from 'components/search.component.js';
+
+const _setSearch = () => {
+
+  const loadNodes = function(nodesDict) {
+    const nodesArray = _.values(nodesDict).filter(node =>
+      node.isAggregated !== true && node.type !== 'COUNTRY' && node.type !== 'LOGISTICS HUB'
+    );
+
+    this.autocomplete.list = nodesArray;
+  };
+
+  const parseNode = (node) => {
+    return {
+      label: node.name.toLowerCase(),
+      value: JSON.stringify({
+        id: node.id,
+        name: node.name.toLowerCase(),
+        columnName: node.type.toLowerCase()
+      })
+    };
+  };
+
+  const onNodeSelected =function() {
+    let url = null;
+    const nodeId = parseInt(JSON.parse(event.text.value).id);
+    const type = JSON.parse(event.text.value).columnName;
+
+    if (type === 'exporter' || type === 'importer') {
+      url = `factsheet-actor.html?nodeId=${nodeId}`;
+    } else {
+      url = `factsheet-place.html?nodeId=${nodeId}`;
+    }
+
+    if (!url) return;
+
+    window.location.href = url;
+  };
+
+  fetch(`${API_URL}/v1/get_all_nodes?&country=brazil&commodity=soy`)
+    .then(response => response.json())
+    .then((result) => {
+
+      document.querySelector('.js-search-container').classList.remove('is-hidden');
+
+      const search = new Search();
+      search.onCreated();
+
+      search.autocomplete.data = parseNode;
+      loadNodes.apply(search, [result.data]);
+      search.callbacks = {};
+      search.callbacks.onNodeSelected = onNodeSelected;
+    });
+};
+
+const _init = () => {
+  _setSearch();
+};
+
+_init();
