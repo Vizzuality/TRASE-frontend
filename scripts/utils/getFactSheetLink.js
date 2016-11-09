4163c685216bd57c50f4d54aba2d94b335ecc963
nerik <erik.escoffier@gmail.com>
2016-11-09 11:55:20 +0100

    fix factsheets links

--- scripts/utils/getFactSheetLink.js
+++ scripts/utils/getFactSheetLink.js
@@ -1,6 +1,6 @@
 // See https://github.com/sei-international/TRASE/issues/112
 export default (nodeId, columnId) => {
-  if ([0,1,2,3,5].indexOf(columnId) > -1) {
+  if ([0,1,2,4,6].indexOf(columnId) > -1) {
     // TODO rename factsheets to factsheet-actor
     const baseURL = ([0,1,2].indexOf(columnId) > -1) ? 'factsheet-place' : 'factsheets';
     return `./${baseURL}.html?nodeId=${nodeId}`;
