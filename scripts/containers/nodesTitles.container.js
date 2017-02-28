5a8643cd4c43e6ad684679a92bf556f71f5ab5df
pjosh <jorgegar@gmail.com>
2017-02-28 13:44:02 +0100

    New node titles design

--- scripts/containers/nodesTitles.container.js
+++ scripts/containers/nodesTitles.container.js
@@ -1,14 +1,28 @@
+import { selectNode } from 'actions/flows.actions';
 import connect from 'connect';
 import NodesTitles from 'components/nodesTitles.component.js';
 
 // this maps component methods to app state updates
 // keys correspond to method names, values to state prop path
 const mapMethodsToState = (state) => ({
-  selectNodes: state.flows.selectedNodesData,
+  selectNodes: {
+    nodesData: state.flows.selectedNodesData,
+    recolorGroups: state.flows.recolorGroups
+  },
   highlightNode: {
     _comparedValue: (state) => state.flows.highlightedNodeData,
-    _returnedValue: (state) => (state.flows.highlightedNodeData.length === 0) ? state.flows.selectedNodesData : state.flows.highlightedNodeData
+    _returnedValue: (state) => {
+      return {
+        nodesData: (state.flows.highlightedNodeData.length === 0) ? state.flows.selectedNodesData : state.flows.highlightedNodeData,
+        isHighlight: state.flows.highlightedNodeData.length > 0,
+        recolorGroups: state.flows.recolorGroups
+      };
+    }
   }
 });
 
-export default connect(NodesTitles, mapMethodsToState);
+const mapViewCallbacksToActions = () => ({
+  onCloseNodeClicked: (id) => selectNode(id)
+});
+
+export default connect(NodesTitles, mapMethodsToState, mapViewCallbacksToActions);
