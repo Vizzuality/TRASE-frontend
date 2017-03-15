f644107d9695bcd3cbb56f26ee657866584f8b38
Erik Escoffier <erik.escoffier@gmail.com>
2017-03-15 17:25:07 +0100

    Feature/map vector paraguay (#226)
    * handle case where get_linked_geoids returns nothing
    
    * added paraguay shape file + processing

--- bin/getVectorMaps.sh
+++ bin/getVectorMaps.sh
@@ -12,4 +12,6 @@ ${BIN}/topojson -p --simplify 0.0000001 -o public/STATE.topo.json -- tmp/STATE.j
 
 ${BIN}/cartodb -u p2cs-sei -f geojson 'SELECT the_geom, geoid FROM brazil_municipalities_1' > tmp/MUNICIPALITY.json
 ${BIN}/topojson -p --simplify 0.0000001 -o public/MUNICIPALITY.topo.json -- tmp/MUNICIPALITY.json
-# ${BIN}/topojson -p --simplify 0.001 -o public/municip.topo.low.json -- tmp/municip.json
+
+ogr2ogr -f GeoJSON -t_srs crs:84 tmp/DEPARTMENT.json bin/shapefiles/paraguay-department/PARAGUAY_ADM1.shp
+${BIN}/topojson -p --simplify 0.0000000001 -o public/DEPARTMENT.topo.json -- tmp/DEPARTMENT.json
