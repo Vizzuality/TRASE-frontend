7646d3804fb2c3055652e5ddae2aa9768840f130
erik <erik.escoffier@gmail.com>
2017-03-29 12:47:09 +0200

    recolor by cleanup

--- scripts/components/nav-flows.component.js
+++ scripts/components/nav-flows.component.js
@@ -1,8 +1,6 @@
 import Dropdown from 'components/dropdown.component';
 import 'styles/components/shared/nav.scss';
-import RecolorByTemplate from 'ejs!templates/flows-nav-context/recolorBy.ejs';
 import Tooltip from 'tether-tooltip';
-import _ from 'lodash';
 
 export default class {
   onCreated() {
@@ -28,58 +26,6 @@ export default class {
     });
   }
 
-  renderContext({ contexts, selectedContextId, tooltips}) {
-    let currentContext = contexts ? contexts.find(elem => elem.id === selectedContextId) : null;
-    let recolorBy = null;
-
-    if (currentContext) {
-      recolorBy = currentContext.recolorBy.sort((a, b) => (a.groupNumber === b.groupNumber) ? (a.position > b.position) : (a.groupNumber > b.groupNumber));
-    }
-
-    document.querySelector('.js-context-recolorBy').innerHTML = RecolorByTemplate({
-      recolorBy: this._generateRecolorByOption(recolorBy),
-      tooltips
-    });
-
-    if (currentContext) {
-      // right side
-      this.recolorByDropdown = new Dropdown('recolor-by', this.callbacks.onRecolorBySelected, false, true);
-
-      this.legendContainer = document.querySelector('.js-dropdown-item-legend-summary');
-    }
-
-  }
-
-  _generateRecolorByOption(recolorBy) {
-    if (recolorBy === null) {
-      return null;
-    }
-
-    return recolorBy.map(recolorByElem => {
-      recolorByElem.label = recolorByElem.label || recolorByElem.name;
-      switch (recolorByElem.legendType) {
-        case 'qual': {
-          recolorByElem.options = [];
-          recolorByElem.nodes.forEach(node => recolorByElem.options.push({
-            class: `-${_.toLower(recolorByElem.type)}-${_.toLower(recolorByElem.legendType)}-${_.toLower(recolorByElem.legendColorTheme)}-${_.toLower(node)}`.replace(/ /g, '-'),
-            label: _.capitalize(node)
-          }));
-          break;
-        }
-        case 'stars':
-        case 'linear': {
-          recolorByElem.options = [];
-          _.range(recolorByElem.intervalCount).forEach(node => recolorByElem.options.push({
-            class: `-${_.toLower(recolorByElem.type)}-${_.toLower(recolorByElem.legendType)}-${_.toLower(recolorByElem.legendColorTheme)}-${_.toLower(node)}`.replace(/ /g, '-'),
-            label: ''
-          }));
-          break;
-        }
-      }
-      return recolorByElem;
-    });
-  }
-
   _setVars() {
     this.el = document.querySelector('.c-nav');
 
@@ -102,28 +48,6 @@ export default class {
   setAppMenuVisibility() {
     this.AppNav.classList.toggle('is-hidden', !this.state.visibilityAppMenu);
     this.FlowsNav.classList.toggle('is-hidden', this.state.visibilityAppMenu);
-  }
-
-  selectRecolorBy(data) {
-    if (!this.recolorByDropdown) {
-      return;
-    }
-    let recolorByValue = data.name;
-
-    const selectedRecolorByLegend = this.recolorByDropdown.el.querySelector(`[data-value="${recolorByValue}"]`);
-    const selectedRecolorByLegendItems = Array.prototype.slice.call(selectedRecolorByLegend.querySelectorAll('.js-dropdown-item-legend li'), 0);
-
-    const legendItems = [];
-    selectedRecolorByLegendItems.forEach(legend => legendItems.push(legend.className));
-    this.legendContainer.innerHTML = legendItems.map(legendItem => `<div class="color ${legendItem}"></div>`).join('');
-    this.recolorByDropdown.selectValue(recolorByValue);
-  }
-
-  updateNodeSelectionColors(colors) {
-    if (colors === null || colors.length === 0) {
-      return;
-    }
-    this.legendContainer.innerHTML = colors.map(color => `<div class="color -flow-${color}" style="order:${color};"></div>`).join('');
   }
 
   selectView(value) {
--- scripts/containers/nav-flows.container.js
+++ scripts/containers/nav-flows.container.js
@@ -1,37 +1,15 @@
 import connect from 'connect';
 import {
-  selectRecolorBy, selectView
+  selectView
 } from 'actions/flows.actions';
 import Nav from 'components/nav-flows.component.js';
 
 const mapMethodsToState = (state) => ({
-  renderContext: {
-    _comparedValue: (state) => state.flows.selectedContext,
-    _returnedValue: (state) => {
-      return {
-        contexts: state.flows.contexts,
-        selectedContextId: state.flows.selectedContextId,
-        tooltips: state.app.tooltips
-      };
-    }
-  },
   addTooltips: state.app.tooltips,
-  selectRecolorBy: state.flows.selectedRecolorBy,
-  selectView: state.flows.detailedView,
-  updateNodeSelectionColors: {
-    _comparedValue: (state) => state.flows.recolorGroups,
-    _returnedValue: (state) => {
-      return (state.flows.selectedRecolorBy === undefined || state.flows.selectedRecolorBy.name === 'none')
-        // TODO state.flows.recolorGroups should probably be cleaned up of all undefined values in the state
-        ? state.flows.recolorGroups.filter(c => c !== undefined) : null;
-    }
-  },
+  selectView: state.flows.detailedView
 });
 
 const mapViewCallbacksToActions = () => ({
-  onRecolorBySelected: value => {
-    return selectRecolorBy(value);
-  },
   onViewSelected: detailedView => selectView(detailedView === 'true')
 });
 
