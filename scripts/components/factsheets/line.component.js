cf04c6e218bc08441c7c7e0ec468f96b9b62605a
pjosh <jorgegar@gmail.com>
2017-04-11 17:01:41 +0200

    Unhardcode line chart

--- scripts/components/factsheets/line.component.js
+++ scripts/components/factsheets/line.component.js
@@ -7,10 +7,7 @@ import {
   scaleLinear as d3_scale_linear,
   scaleTime as d3_scale_time
 } from 'd3-scale';
-import {
-  extent as d3_extent,
-  max as d3_max
-} from 'd3-array';
+import { extent as d3_extent } from 'd3-array';
 import {
   line as d3_line,
   area as d3_area
@@ -22,26 +19,70 @@ import 'styles/components/factsheets/line.scss';
 
 
 export default class {
-  constructor(className, trajectory_deforestation) {
+  constructor(className, data) {
     const elem = document.querySelector(className);
     const margin = {top: 30, right: 40, bottom: 30, left: 94};
     const width = elem.clientWidth - margin.left - margin.right;
     const height = 425 - margin.top - margin.bottom;
-    const allValues = [...trajectory_deforestation.lines[0].values
-      , ...trajectory_deforestation.lines[1].values
-      , ...trajectory_deforestation.lines[2].values
-      , ...trajectory_deforestation.lines[3].values];
+    let allValues = [];
+
+    let container = d3_select(elem)
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     let x = d3_scale_time()
       .range([0, width])
-      .domain(d3_extent(trajectory_deforestation.includedYears, y => new Date(y, 0)));
+      .domain(d3_extent(data.includedYears, y => new Date(y, 0)));
+
+    data.lines.forEach((lineData) => {
+      allValues = [...allValues, ...lineData.values];
+      const y0 = d3_scale_linear()
+        .rangeRound([height, 0])
+        .domain(d3_extent(lineData.values));
+      const lineValuesWithFormat = prepareData(data.includedYears, lineData);
+      let area = null,
+        line = null;
+
+      switch (lineData.type) {
+        case 'area':
+          area = d3_area()
+            .x(d => x(d.date))
+            .y0(height)
+            .y1(d => y0(d.value));
+
+          line = d3_line()
+            .x(d => x(d.date))
+            .y(d => y0(d.value));
+
+          container.append('path')
+            .datum(lineValuesWithFormat)
+            .attr('class', lineData.style)
+            .attr('d', area);
+
+          container.append('path')
+            .datum(lineValuesWithFormat)
+            .attr('class', `line-${lineData.style}`)
+            .attr('d', line);
+          break;
+        case 'line':
+          line = d3_line()
+            .x(d => x(d.date))
+            .y(d => y0(d.value));
+
+          container.append('path')
+            .datum(lineValuesWithFormat)
+            .attr('class', lineData.style)
+            .attr('d', line);
+          break;
+      }
+    });
+
     let y = d3_scale_linear()
       .rangeRound([height, 0])
       .domain(d3_extent([0, ...allValues]));
-    let y0 = d3_scale_linear().rangeRound([height, 0]);
-    let y1 = d3_scale_linear().rangeRound([height, 0]);
-    let y2 = d3_scale_linear().rangeRound([height, 0]);
-    let y3 = d3_scale_linear().rangeRound([height, 0]);
 
     const xAxis = d3_axis_bottom(x)
       .tickSize(0)
@@ -62,84 +103,10 @@ export default class {
         const format = d3_format('0');
 
         if (i === 6) {
-          return `${format(value)}${trajectory_deforestation.unit}`;
+          return `${format(value)}${data.unit}`;
         }
         return format(value) ;
       });
-
-    let container = d3_select(elem)
-      .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-          .attr('transform',
-                'translate(' + margin.left + ',' + margin.top + ')');
-
-    y0.domain(d3_extent(trajectory_deforestation.lines[0].values));
-    y1.domain(d3_extent(trajectory_deforestation.lines[1].values));
-    y2.domain(d3_extent(trajectory_deforestation.lines[2].values));
-    y3.domain(d3_extent(trajectory_deforestation.lines[3].values));
-
-    const dataDeforestation = prepareData(trajectory_deforestation.includedYears, trajectory_deforestation.lines[0]);
-    const dataPotentialDeforestation = prepareData(trajectory_deforestation.includedYears, trajectory_deforestation.lines[1]);
-    const dataTerritorialDeforestation = prepareData(trajectory_deforestation.includedYears, trajectory_deforestation.lines[2]);
-    const dataStateAverage = prepareData(trajectory_deforestation.includedYears, trajectory_deforestation.lines[3]);
-
-    const area0 = d3_area()
-      .x(d => x(d.date))
-      .y0(height)
-      .y1(d => y0(d.value)),
-
-      line0 = d3_line()
-        .x(d => x(d.date))
-        .y(d => y0(d.value)),
-
-      line1 = d3_line()
-        .x(d => x(d.date))
-        .y(d => y1(d.value)),
-
-      area2 = d3_area()
-        .x(d => x(d.date))
-        .y0(height)
-        .y1(d => y2(d.value)),
-
-      line2 = d3_line()
-        .x(d => x(d.date))
-        .y(d => y2(d.value)),
-
-      line3 = d3_line()
-        .x(d => x(d.date))
-        .y(d => y3(d.value));
-
-    container.append('path')
-      .datum(dataDeforestation)
-      .attr('class', 'area-pink')
-      .attr('d', area0);
-
-    container.append('path')
-      .datum(dataDeforestation)
-      .attr('class', 'line-pink')
-      .attr('d', line0);
-
-    container.append('path')
-      .datum(dataPotentialDeforestation)
-      .attr('class', 'line-dashed-pink')
-      .attr('d', line1);
-
-    container.append('path')
-      .datum(dataTerritorialDeforestation)
-      .attr('class', 'area-black')
-      .attr('d', area2);
-
-    container.append('path')
-      .datum(dataTerritorialDeforestation)
-      .attr('class', 'line-black')
-      .attr('d', line2);
-
-    container.append('path')
-      .datum(dataStateAverage)
-      .attr('class', 'line-dashed-black')
-      .attr('d', line3);
 
     container.append('g')
       .attr('transform', `translate(0, ${height} )`)
