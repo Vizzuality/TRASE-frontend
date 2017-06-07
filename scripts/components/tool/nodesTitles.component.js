38183769ac9a7707d80c6d2f8c862511e7ccedee
erik <erik.escoffier@gmail.com>
2017-06-07 19:43:11 +0200

    no link for unknown nodes

--- scripts/components/tool/nodesTitles.component.js
+++ scripts/components/tool/nodesTitles.component.js
@@ -35,7 +35,9 @@ export default class {
 
   _update(isSelect, nodesData, recolorGroups = null) {
     this.el.innerHTML = NodeTitleTemplate({
-      nodes: nodesData,
+      nodes: nodesData.map(node => {
+        return Object.assign(node, {}, { hasLink: node.isUnknown !== true && node.profileType !== undefined && node.profileType !== null })
+      }),
       isSelect: isSelect || nodesData.length > 1,
       recolorGroups: recolorGroups
     });
