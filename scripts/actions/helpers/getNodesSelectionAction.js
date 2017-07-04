1e44edb1551a483c0b4c81efd0aab21ac87fdb7a
erik <erik.escoffier@gmail.com>
2017-07-04 13:27:43 +0200

    fixed code style

--- scripts/actions/helpers/getNodesSelectionAction.js
+++ scripts/actions/helpers/getNodesSelectionAction.js
@@ -38,7 +38,11 @@ const getNodeSelectedMeta = (selectedMapDimension, node, selectedResizeByLabel, 
     return meta;
   } else if (visibleNode && visibleNode.quant && meta.rawValue !== visibleNode.quant && NODE_ENV_DEV === true) {
     // See https://basecamp.com/1756858/projects/12498794/todos/312319406
-    console.warn('Attempting to show different values two dimensions with the same name.', 'ResizeBy: ' + selectedResizeByLabel + ' with value ' + visibleNode.quant, 'Map layer: ' + meta.name + ' with value ' + meta.rawValue);
+    console.warn(
+      'Attempting to show different values two dimensions with the same name.',
+      'ResizeBy: ' + selectedResizeByLabel + ' with value ' + visibleNode.quant,
+      'Map layer: ' + meta.name + ' with value ' + meta.rawValue
+    );
   }
   return null;
 };
@@ -56,7 +60,10 @@ const getSelectedNodesData = (selectedNodesIds, visibleNodes, nodesDictWithMeta,
     if (nodesDictWithMeta) {
       node = Object.assign(node, nodesDictWithMeta[nodeId]);
       // add metas from the map layers to the selected nodes data
-      node.selectedMetas = _.compact([getNodeSelectedMeta(selectedMapDimensions[0], node, selectedResizeByLabel, visibleNode), getNodeSelectedMeta(selectedMapDimensions[1], node, selectedResizeByLabel, visibleNode),]);
+      node.selectedMetas = _.compact([
+        getNodeSelectedMeta(selectedMapDimensions[0], node, selectedResizeByLabel, visibleNode),
+        getNodeSelectedMeta(selectedMapDimensions[1], node, selectedResizeByLabel, visibleNode)
+      ]);
     }
 
     if (visibleNode) {
