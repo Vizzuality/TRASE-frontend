5bf1887329a74e60cc0df8a8d7c3b6e3a8f4e375
erik <erik.escoffier@gmail.com>
2017-09-18 10:44:41 +0200

    fixed empty flows handling

--- scripts/containers/tool/tool-content.container.js
+++ scripts/containers/tool/tool-content.container.js
@@ -9,10 +9,11 @@ const mapMethodsToState = (state) => ({
   toggleError: {
     _comparedValue: (state) => state.tool.links,
     _returnedValue: (state) => {
-      // return (state.tool.links && links.length === 0)
+      // console.log(state)
       // TODO hack, fix needed on the API, see https://basecamp.com/1756858/projects/12498794/todos/323528801
-      const has2015 = state.tool.selectedYears[0] <= 2015 && state.tool.selectedYears[1] >= 2015;
-      return !state.tool.links || state.tool.links.length === 0 || (state.tool.selectedResizeByName === 'AGROSATELITE_SOY_DEFOR_' && has2015);
+      // Note: only start date is required to be checked as we disabled multi year selection
+      // return state.tool.selectedResizeBy.name === 'AGROSATELITE_SOY_DEFOR_' && (state.tool.selectedYears[0] === 2015 || state.tool.selectedYears[0] === 2016);
+      return state.tool.links === null;
     }
   }
 });
